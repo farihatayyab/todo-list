a4ea27b67b8c68e56b7221697b484961b8d655e4
Fariha Tayyab <farihatayyab13@gmail.com>
2025-09-19 19:18:15 +0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,70 @@
 <template>
 <div id="app">
-  <h1>📝 Todo List</h1>
-<input type="text" placeholder="add todo" v-model="newTask" @keyup.enter="addTask"/>
-<button @click="addtask">add</button>
+    <h1>📝 Todo List</h1>
+    <input type="text" placeholder="add todo" v-model="newTask" @keyup.enter="addTask" />
+    <button @click="addTask">add</button>
 
-<ul>
-   <li v-for="(task, index) in tasks" :key="index">
-   <span :class="{ done: task.done }" @click="toggleTask(task)">
-          {{ task.text }}
-        </span>
-        <button @click="removetask(index)">remove</button>
-   </li>
+    <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+            <span :class="{ done: task.done }" @click="toggleTask(task)">
+                {{ task.text }}
+            </span>
+            <button @click="removeTask(index)">remove</button>
+        </li>
 
+    </ul>
+</div>
+</template>
 
+<script setup>
+import { ref } from 'vue';
 
+// Reactive state
+const newTask = ref('');
+const tasks = ref([]);
 
-</ul>
-  </div>
-  </template>
-
-
-<script>
-
-export default {
-  name: 'App',
-  data(){
-  return{
-    newTask: '',
-    tasks: []
+// Methods
+function addTask() {
+  if (newTask.value.trim()) {
+    tasks.value.push({ text: newTask.value, done: false });
+    newTask.value = '';
   }
-  },
-  methods:{
-    addtask(){
-      if(this.newTask.trim()){
-        this.tasks.push({text :this.newTask,done:false})
-      }
-      
-    },
-      toggleTask(task) {
-      task.done = !task.done;
-    },
-    removetask(index){
-      this.tasks.splice(index,1)
-    }
-  }
-};
- 
- 
+}
 
+function toggleTask(task) {
+  task.done = !task.done;
+}
+
+function removeTask(index) {
+  tasks.value.splice(index, 1);
+}
 </script>
 
 <style>
 .app {
-  max-width: 400px;
-  margin: 50px auto;
-  text-align: center;
-  font-family: Arial, sans-serif;
+    max-width: 400px;
+    margin: 50px auto;
+    text-align: center;
+    font-family: Arial, sans-serif;
 }
+
 ul {
-  list-style: none;
-  padding: 0;
+    list-style: none;
+    padding: 0;
 }
+
 li {
-  margin: 5px 0;
-  padding: 8px;
-  background: #f4f4f4;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 6px;
+    margin: 5px 0;
+    padding: 8px;
+    background: #f4f4f4;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 6px;
 }
+
 .done {
-  text-decoration: line-through;
-  color: gray;
+    text-decoration: line-through;
+    color: gray;
 }
 </style>
-
-
